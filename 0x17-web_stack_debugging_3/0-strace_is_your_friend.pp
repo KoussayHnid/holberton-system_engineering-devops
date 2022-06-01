94ad8600ea48eec138a8690ad995fc5107b34980
koussay557 <henidkousay@gmail.com>
2022-06-01 10:50:04 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 #Using strace, find out why Apache is returning a 500 error.
 #strace can attach to a current running process
-exec{'fix_wordpress':
+exec {'fix_wordpress':
  command => "sed -i 's/phpp/php/' /var/www/html/wp-setting.php",
  path    => '/bin',
+}
